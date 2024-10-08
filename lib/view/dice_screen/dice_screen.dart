5f93d415b3d9bfa93bed3a704d19effa9caaf705
theertha54321 <theerthavinodvinod@gmail.com>
2024-10-08 08:42:00 +0530

    first commit

---
+++ lib/view/dice_screen/dice_screen.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class DiceScreen extends StatefulWidget {
+  const DiceScreen({super.key});
+
+  @override
+  State<DiceScreen> createState() => _DiceScreenState();
+}
+
+class _DiceScreenState extends State<DiceScreen> {
+
+List dices = [
+  "assets/images/d1.png",
+  "assets/images/d2.png",
+  "assets/images/d3.png",
+  "assets/images/d4.png",
+  "assets/images/d5.png",
+  "assets/images/d6.png",
+  
+];
+int? randomNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: InkWell(
+          onTap: (){
+            randomNumber = Random().nextInt(6);
+            print(randomNumber);
+            setState(() {
+              
+            });
+          },
+          child: Image.asset(
+            height: 200,
+            width: 200,
+            randomNumber == null? "assets/images/button.png" : dices[randomNumber!]
+           
+          )
+    ),
+      ),
+    );
+  }
+}
